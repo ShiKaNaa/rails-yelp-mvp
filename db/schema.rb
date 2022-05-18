7ce35f7c61895c367e5c14ec1240a6fe63233ec7
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-05-18 10:50:49 +0200

    controllers added

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_18_084525) do
+ActiveRecord::Schema.define(version: 2022_05_18_084750) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
-    t.string "adress"
+    t.string "address"
     t.string "phone_number"
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
